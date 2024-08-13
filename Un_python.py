942cc112aeb9dbc0a86cc824309c4c2935c1b769
gilem <andreovel205@gmail.com>
2024-08-13 18:23:04 -0500

    This_is_first_commit

---
+++ Un_python.py
@@ -0,0 +1,8 @@
+#The moment a person starts programing in python.
+#from readline import append_history_file
+This_is_a_list = [2,3,5,6,63,4,34,344]
+This_is_a_list.extend([4334,234,346,5665])
+Una_summatory = sum(This_is_a_list)
+print("Result of the summatory from the data =", Una_summatory)
+
+
