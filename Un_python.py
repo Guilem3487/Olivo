b84a3740b52749424c59fe54fa557f408a560fb7
gilem <andreovel205@gmail.com>
2024-08-13 19:08:59 -0500

    I've separated the functions

--- Un_python.py
+++ Un_python.py
@@ -1,10 +1,15 @@
 #The moment a person starts programing in python.
-#from readline import append_history_file
+
 This_is_a_list = [2,3,5,6,63,4,34,344]
         #####This is the next step, because this is the start of an application.
-user_input1=int(input("Enter numbers to the list: "))
-This_is_a_list.append(user_input1)
+try:
+    user_input1= int(input("Enter numbers to the list: "))
+    This_is_a_list.append(user_input1)
+except ValueError:
+    print("Enter only numbers")
+
 This_is_a_list.extend([4334,234,346,5665])
+
 Una_summatory = sum(This_is_a_list)
 print("Result of the summatory from the data =",Una_summatory)
 print(Una_summatory)
